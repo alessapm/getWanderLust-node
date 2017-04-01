1f690e632511351dd600904a087f5e5773bd618b
Alessa Messineo <apmessineo@gmail.com>
2017-04-01 18:09:13 -0400

    expand image search to 50

--- services/getty_images.js
+++ services/getty_images.js
@@ -5,7 +5,7 @@ const gettyImagesKey = process.env.Getty_Key;
 
 
 gettyImages.search = (city) => {
-  return fetch(`https://api.gettyimages.com/v3/search/images/creative?phrase=${city}&page=1&page_size=32&orientations=Horizontal`, {
+  return fetch(`https://api.gettyimages.com/v3/search/images/creative?phrase=${city}&page=1&page_size=56&orientations=Horizontal`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
